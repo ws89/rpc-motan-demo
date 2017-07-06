d12e4aab48a724aa7b4151ae5ca613870d460475
ws <mkwork@qq.com>
2017-07-06 23:18:21 +0800

    RpcContext

--- motan/src/main/java/io/github/ws89/demo/motan/service/impl/ProviderServiceImpl.java
+++ motan/src/main/java/io/github/ws89/demo/motan/service/impl/ProviderServiceImpl.java
@@ -1,5 +1,6 @@
 package io.github.ws89.demo.motan.service.impl;
 
+import com.weibo.api.motan.rpc.RpcContext;
 import io.github.ws89.demo.motan.service.ProviderService;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ public class ProviderServiceImpl implements ProviderService {
 
     public String hello(String name) {
         System.out.println(name + " invoked rpc service.");
+        RpcContext rpcContext = RpcContext.getContext();
 
         return "hello " + name;
     }
